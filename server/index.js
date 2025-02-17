055b1bb6007b17e3ab2ac20f12004dc15566a6d3
Phoenix <aadilsiddiqui2016+github@gmail.com>
2025-02-17 19:57:24 +0530

    Site made responsive and auth issue solved

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET, 
-  process.env.GOOGLE_REDIRECT_URI
+  process.env.CLIENT_URL
 );
 
 // Middleware
