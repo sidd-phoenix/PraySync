2c3971a76c85d02238ad5b4460e0188b95ec0d68
Phoenix <aadilsiddiqui2016+github@gmail.com>
2025-02-17 21:11:57 +0530

    Site made responsive and auth issue solved

--- server/index.js
+++ server/index.js
@@ -81,7 +81,6 @@ app.get('/auth/google', (req, res) => {
 // Handle the callback from Google
 app.post('/auth/google/callback', async (req, res) => {
   console.log("callback")
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   // console.log(req.body)
   const { code } = req.body;
   const { tokens } = await client.getToken(code);
